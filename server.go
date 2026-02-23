753e3194b231ca282ff0201784be049592d6973d
Raghav Sood <raghavsood3678@gmail.com>
2026-02-23 17:23:55 +0530

    Validation logic added for server

--- server.go
+++ server.go
@@ -8,6 +8,9 @@ import (
 	ratelimiterpb "github.com/Voltac209/api_rate_limiter/gen/ratelimiter/v1"
 	ratelimiter "github.com/Voltac209/api_rate_limiter/src"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type RateLimiterServer struct {
@@ -19,6 +22,15 @@ func (s *RateLimiterServer) Check(
 	ctx context.Context,
 	req *ratelimiterpb.RateLimitRequest,
 ) (*ratelimiterpb.RateLimitResponse, error) {
+	if req.Key == "" {
+		return nil, status.Error(codes.InvalidArgument, "key cannot be empty")
+	}
+	if req.Limit == 0 {
+		return nil, status.Error(codes.InvalidArgument, "limit must be greater than 0")
+	}
+	if req.WindowSeconds == 0 {
+		return nil, status.Error(codes.InvalidArgument, "windowSeconds must be greater than 0")
+	}
 	result := s.limiter.Check(
 		req.Key,
 		req.Limit,
@@ -51,8 +63,10 @@ func main() {
 	)
 
 	log.Println("RateLimiter gRPC server running on :50051")
+	reflection.Register(grpcServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to server: %v", err)
 	}
+
 }
